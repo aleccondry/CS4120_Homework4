3b587a9cb94fef07799cb3ed5a0d93c4a18441e2
Shrihari Subramaniam <subramaniam.s@northeastern.edu>
2023-10-24 18:19:58 -0400

    new utils function, finished writeup

--- sentiment_utils.py
+++ sentiment_utils.py
@@ -66,6 +66,38 @@ Make sure that you properly update any docstrings as needed.
 """
 
 
+def generate_lines_from_file(training_file_path: str) -> list:
+    """
+    Generates data from file formated like:
+
+    tokenized text from file: ["...", "...", ...]
+    labels: [0, 1, 0, 1, ...]
+    
+    Parameters:
+        training_file_path - str path to file to read in
+    Return:
+        a list of lists of tokens and a list of int labels
+    """
+    # PROVIDED
+    f = open(training_file_path, "r", encoding="utf8")
+    X = []
+    y = []
+    for review in f:
+        if len(review.strip()) == 0:
+            continue
+        dataInReview = review.strip().split("\t")
+        if len(dataInReview) != 3:
+            continue
+        else:
+            t = tuple(dataInReview)
+            if (not t[2] == '0') and (not t[2] == '1'):
+                print("WARNING")
+                continue
+            X.append(t[1])
+            y.append(int(t[2]))
+    f.close()  
+    return X, y
+
 def get_prfa(dev_y: list, preds: list, verbose=False) -> tuple:
     """
     Calculate precision, recall, f1, and accuracy for a given set of predictions and labels.
